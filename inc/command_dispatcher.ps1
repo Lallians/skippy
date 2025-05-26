0230275daa99021ff14301d55f964a502d624b80
Lallians <contact@lallians-creation.fr>
2025-05-26 22:40:54 +0200

    rearrange code in split files

---
+++ inc/command_dispatcher.ps1
@@ -0,0 +1,79 @@
+﻿# allows skippy to parse CLI arguments
+# returns an array of $arg => $value
+function parseArguments {
+    param(
+        [string[]]$RemainingArgs
+    )
+
+    $parsed = @{}
+    for ($i = 0; $i -lt $RemainingArgs.Count; $i++) {
+
+        # If the tested string starts with '-', we know we want to assign a value with a key named after the stirng.
+        if ($RemainingArgs[$i] -like "-*") {
+            $key = $RemainingArgs[$i].TrimStart("-")
+
+            # We check if the following string does not start with '-', which mean the user provided a value
+            # if no value is provided, we assume the parameter is a flag, ie a boolean set to true
+            if ($i + 1 -lt $RemainingArgs.Count) {
+                # there is indeed a following string
+
+                # we check whether it is a value for our parameter or a flag
+                if($RemainingArgs[$i + 1] -notlike "-*") {
+                    $i++; # increment so that we check the next parameter
+                    $value = $RemainingArgs[$i]
+                } else {
+                    $value = $true
+                }
+
+            }
+            
+            $parsed[$key] = $value
+        } else {
+
+            # the argument was not named, so we just fill with whatever value was passed
+            $parsed[$i] = $RemainingArgs[$i]
+
+        }
+
+    }
+
+    return $parsed
+}
+
+
+
+
+# Function that returns a string of argument to use in a dynamically generated function call
+# Takes an array or argument names and a set of value. the set of values must be a map with the same keys as the argument name list.
+function getArgsFormatted {
+    param (
+        [Parameter(Mandatory = $true)]
+        [hashtable]$fnArgs,
+
+        [Parameter(Mandatory = $true)]
+        [hashtable]$parsedArgs
+    )
+
+    # Use what the user provided
+    $paramsFormatted = @{}
+    foreach($theKey in $fnArgs.Keys) {
+        if($parsedArgs.ContainsKey($theKey)) {
+            $paramsFormatted[$theKey] = $parsedArgs[$theKey]
+        }
+    }
+    
+    # Join the parameters into a string: -param1 'value1' -param2 'value2'
+    $argString = ($paramsFormatted.GetEnumerator() | ForEach-Object {
+        # no quotes for bool values
+        if( ($_.Value -eq 0) -or ($_.Value -eq $false) ) {
+            "-$($_.Key) `$false"
+        } elseif( ($_.Value -eq 1) -or ($_.Value -eq $true) ) {
+            "-$($_.Key) `$true"
+        } else {
+            "-$($_.Key) '$($_.Value)'"
+        }
+    }) -join ' '
+
+    return $argString
+
+}
---
+++ inc/projects.ps1
@@ -0,0 +1,78 @@
+﻿function getProjectsPath {
+    return Join-Path (getConf 'dockerPath') 'projects'
+}
+
+# Returns the path of a given project
+function getProjectPath {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$appName
+    )
+
+    return Join-Path (getProjectsPath) $appName 
+
+}
+
+# Returns an array of the available projects with relevant infos.
+function getAvailableProjects {
+
+    $availableProjects = @{}
+    $projectsPath = getProjectsPath
+
+    $folders = Get-ChildItem $projectsPath
+    if($folders.Count -ile 0) {
+        return $availableProjects
+    }
+
+    foreach ($folder in $folders) {
+        $folderName = $folder.Name
+    
+        # Check if a Docker container with that name exists (running or stopped)
+        # We must go to the Docker directory to be able to do that
+
+        $containers = docker ps -a --filter "name=$folderName$" --format "{{.Names}}"
+        if ($containers.Count -gt 0) {
+            # We found at least one container so we know a project exists in docker
+            # Let's gather some stats
+            # TODO: Gather more stats :)
+
+            # Check if all services are running
+            $running_containers = 0
+            foreach($container in $containers) {
+                $status = (docker inspect -f '{{.State.Status}}' $container)
+                if($status -eq 'running') {
+                    $running_containers++
+                }
+            }
+            # Add some quick visuals
+            if($running_containers -eq 0) {
+                $emoji = '❌'
+                $status_message = 'Not running'
+            } elseif($running_containers -eq $containers.Count) {
+                $emoji = '✅'
+                $status_message = 'OK'
+            } else {
+                # some containers are running but not all
+                $emoji = '⚠️'
+                $status_message = 'Need attention'
+            }
+
+            $availableProjects[$folder.Name] = @{
+                'status' = "$emoji $status_message"
+                'services_running' = ($running_containers -as [string]) + '/' + ($containers.Count -as [string]) + ' running'
+                'integrity' = 'OK'
+                'running' = ($running_containers -eq $containers.Count)
+            }
+        } else {
+            $availableProjects[$folder.Name] = @{
+                'status' = '⚠️ Need attention'
+                'services_running' = '0/0'
+                'integrity' = 'Files found on disk but there is no container assigned.'
+                'running' = $false
+            }
+        }
+    }
+
+    return $availableProjects
+
+}
